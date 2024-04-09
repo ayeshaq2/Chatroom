f4862ff3a513311b2c8648858a73f629ae4764b1
Nada <nmurad4@uwo.ca>
2024-04-09 16:10:59 -0400

    client connections and data

--- client.py
+++ client.py
@@ -3,12 +3,14 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 from PyQt5.QtCore import Qt
 import socket
 import json
+import threading
 
 class Client(QWidget):
     def __init__(self, id):
         super().__init__()
         self.id = id
         self.init_ui()
+        self.client_socket = None
 
     def init_ui(self):
         self.setWindowTitle('Frontend')
@@ -20,32 +22,57 @@ class Client(QWidget):
 
     def send_message(self, message):
         try:
-            groupchat_id = 1
-            data={
-                "name":self.id,
-                "message":message,
-                "groupchat_id":groupchat_id
+            groupchat_id = "GC"
+            data = {
+                "name": self.id,
+                "message": message,
+                "groupchat_id": groupchat_id
             }
-            # serialize the dictionary to json format
+            # Serialize the dictionary to json format
             json_data = json.dumps(data)
-            # Create a socket object
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # Connect to the backend
-            client_socket.connect(('localhost', 12349))
             # Send message to the backend
-            client_socket.sendall(json_data.encode())
-            # Receive response from the backend
-            response = client_socket.recv(1024)
-            print(f'Received from backend: {response.decode()}')
+            print('Sending...')
+            self.client_socket.sendall(json_data.encode())
         except Exception as e:
             print(f'Error: {e}')
-        finally:
-            client_socket.close()
+        # finally:
+        #     self.client_socket.close()
+    
+    #receive data
+    def receive_message(self):
+        try:
+            while True:
+                response = self.client_socket.recv(1024)
+                if response:
+                    print(f'Receieved from backend: {response.decode()}')
+                # else:
+                #     print("Connection closed by server")
+                #     break
+        except Exception as e:
+            print(f'Error receiving message: {e}')
+        # finally:
+        #     self.client_socket.close()
+    def connect_to_server(self):
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect(('localhost', 12349))
+            #thread to receive messages
+            receive_thread = threading.Thread(target=self.receive_message)
+            receive_thread.daemon = True
+            receive_thread.start()
+        except Exception as e:
+            print(f'Error connecting to server: {e}')
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     client = Client('nada')
     client.show()
-    message = input('Type a message to send: ')
-    client.send_message(message)
+    client.connect_to_server()
+    while(True):
+        message = input('Type a message to send: ')
+        if (message == 'exit'):
+            sys.exit()
+        client.send_message(message)
     sys.exit(app.exec_())
--- server.cpp
+++ server.cpp
@@ -8,14 +8,131 @@
 #include <queue>
 #include <chrono>
 #include <thread>
+#include <unordered_map>
+#include <vector>
 #include <json/json.h> // JSON library
 
 std::mutex mtx; //mutex for access to message queue
 std::queue<std::string> messageQ; //queue storing messages
 
+//structure for groupchats
+struct GroupChat{
+    std::vector<int> participants;
+};
+
+//groupchat class
+class GroupChats{
+    //constructor
+    public:
+        std::unordered_map<std::string, GroupChat> groupChats;
+    //methods
+    public:
+        //new groupchat
+        void createGroupChat(const std::string& groupChatName, const std::vector<int>& initialParticipant){
+            groupChats[groupChatName] = {initialParticipant};
+        }
+
+        //add participant to groupchat
+        void addParticipant(const std::string& groupChatName, int participant){
+            groupChats[groupChatName].participants.push_back(participant);
+        }
+};
+//initialize groupchats
+GroupChats groupChatList;
+
+
+//send messages
+void sendMessage(){
+    //access the queue, send message based on groupchat
+    while (true){
+        mtx.lock();
+        if(!messageQ.empty()){
+            //get message
+            std::string messageDetails = messageQ.front();
+            //remove from queue
+            messageQ.pop();
+            //print message
+            std::cout << "From queue: " << messageDetails << std::endl;
+            //send message to specific groupchat
+            //parse into json
+            Json::Value messageJson;
+            Json::Reader reader;
+            if (reader.parse(messageDetails, messageJson)) {
+                std::string groupchatId = messageJson["groupchat_id"].asString();
+                //check participants
+                const std::vector<int>& participants = groupChatList.groupChats[groupchatId].participants;
+                //for each participant, send message
+                for (int client : participants){
+                    std::cout << "Sent to client: " << client << std::endl;
+                    send(client, messageJson["message"].asString().c_str(), messageJson["message"].asString().size() + 1, 0);
+                }
+            } else {
+                std::cerr << "Error parsing message JSON: " << reader.getFormattedErrorMessages() << std::endl;
+            }
+
+        }
+        mtx.unlock();
+    }
+
+}
+
 //method to receive messages
+void receiveMessage(int clientSocket){
+    // Buffer to store received data
+    char buf[4096];
+    memset(buf, 0, 4096);
+
+    // Receive JSON message from client
+    ssize_t bytesReceived = recv(clientSocket, buf, 4096, 0);
+    if (bytesReceived == -1) {
+        std::cerr << "Error in recv()" << std::endl;
+        close(clientSocket);
+        return;
+    }
+    
+    // Parse received JSON data
+    Json::CharReaderBuilder builder;
+    Json::Value jsonData;
+    std::string parseErrors;
+    std::istringstream jsonStream(std::string(buf, bytesReceived));
+    if (!Json::parseFromStream(builder, jsonStream, &jsonData, &parseErrors)) {
+        std::cerr << "Error parsing JSON: " << parseErrors << std::endl;
+        close(clientSocket);
+        return;
+    }
+
+    // Extract data from JSON
+    std::string clientName = jsonData["name"].asString();
+    std::string message = jsonData["message"].asString();
+    std::string clientId = jsonData["groupchat_id"].asString();
+    //add socket to data
+    jsonData["client_socket"] = clientSocket;
+
+    // Print received data
+    std::cout << "Received JSON from frontend:" << std::endl;
+    std::cout << "Client Name: " << clientName << std::endl;
+    std::cout << "Message: " << message << std::endl;
+    std::cout << "Groupchat ID: " << clientId << std::endl;
+
+    std::string res = "Hello fro,m backend";
+    // send(clientSocket, res.c_str(), res.size() + 1, 0);
+    // std::cout << "SOCKET: " << clientSocket << std::endl;
+    //add data to queue
+    //lock mutex
+    mtx.lock();
+    //add message
+    messageQ.push(jsonData.toStyledString());
+    //unlock
+    mtx.unlock();
+    //sendMessage();
+}
+
 
 int main() {
+    //test-> initialize groupchat
+    groupChatList.createGroupChat("GC", {});
+    //initialize sender method thread
+    std::thread senderThread(sendMessage);
     // Create a socket
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1) {
@@ -40,58 +157,23 @@ int main() {
         return -1;
     }
 
-    // Wait for a connection
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-    if (clientSocket == -1) {
-        std::cerr << "Error: Can't accept client connection!" << std::endl;
-        return -1;
+    //infinite loop
+    while (true){
+        // Wait for a connection
+        sockaddr_in client;
+        socklen_t clientSize = sizeof(client);
+        int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+        std::cout << "Intial client socket: " << clientSocket << std::endl;
+        if (clientSocket == -1) {
+            std::cerr << "Error: Can't accept client connection!" << std::endl;
+            return -1;
+        }
+        //add client to groupchat
+        groupChatList.addParticipant("GC", clientSocket);
+        //when receive message, handle it
+        receiveMessage(clientSocket);
+        //close(clientSocket);
     }
-
-    // Buffer to store received data
-    char buf[4096];
-    memset(buf, 0, 4096);
-
-    // Receive JSON message from client
-    ssize_t bytesReceived = recv(clientSocket, buf, 4096, 0);
-    if (bytesReceived == -1) {
-        std::cerr << "Error in recv()" << std::endl;
-        close(clientSocket);
-        close(listening);
-        return -1;
-    }
-
-    // Parse received JSON data
-    Json::CharReaderBuilder builder;
-    Json::Value jsonData;
-    std::string parseErrors;
-    std::istringstream jsonStream(std::string(buf, bytesReceived));
-    if (!Json::parseFromStream(builder, jsonStream, &jsonData, &parseErrors)) {
-        std::cerr << "Error parsing JSON: " << parseErrors << std::endl;
-        close(clientSocket);
-        close(listening);
-        return -1;
-    }
-
-    // Extract data from JSON
-    std::string clientName = jsonData["name"].asString();
-    std::string message = jsonData["message"].asString();
-    int clientId = jsonData["groupchat_id"].asInt();
-
-    // Print received data
-    std::cout << "Received JSON from frontend:" << std::endl;
-    std::cout << "Client Name: " << clientName << std::endl;
-    std::cout << "Message: " << message << std::endl;
-    std::cout << "Groupchat ID: " << clientId << std::endl;
-
-    // Send response to the frontend
-    std::string response = "Hello from backend!";
-    send(clientSocket, response.c_str(), response.size() + 1, 0);
-
-    // Close the socket
-    close(clientSocket);
     close(listening);
-
     return 0;
 }
