2fb208a84c6f7550b4d620f1b28e0268bfd828cd
ayeshaq2 <ayeshaqaisar002@gmail.com>
2024-04-26 19:38:34 -0400

    forgot to add the heading back

--- client.py
+++ client.py
@@ -57,6 +57,13 @@ class Client(QWidget):
         self.input_field.returnPressed.connect(self.send_message)
         layout.addWidget(self.input_field)
 
+        self.header_label = QLabel("Chatroom")
+        self.header_label.setAlignment(Qt.AlignCenter)
+        font = self.header_label.font()
+        font.setPointSize(16)
+        self.header_label.setFont(font)
+        layout.addWidget(self.header_label)
+
         self.leave_chatroom_button = QPushButton("Leave Chatroom")
         self.leave_chatroom_button.clicked.connect(self.leave_chat_room)
         layout.addWidget(self.leave_chatroom_button)
@@ -79,6 +86,7 @@ class Client(QWidget):
             self.gc = None
             self.message_display.clear()
             self.chat_room_selector.setCurrentIndex(0)
+            self.header_label = QLabel("Chatroom")
             QMessageBox.information(self, "Chat Room", "You have left the chatroom.")
         else:
             QMessageBox.warning(self, "Chat Room", "You are not in any chat room")
